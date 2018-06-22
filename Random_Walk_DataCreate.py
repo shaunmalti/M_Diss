7b717486952243b5450af26edf3577a024ccaebd
Shaun Markham <shaunmarkham30@gmail.com>
2018-06-22 14:21:22 +0100

    created one file to generate random walk dataset, to be used in random_walk_gan which is a gan for random walk

---
+++ Random_Walk_DataCreate.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import pandas as pd
+# import pandas_datareader.data as web
+import numpy as np
+
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+# from arch import arch_model
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+#matplotlib inline
+
+# Random Walk without a drift
+np.random.seed(1)
+n_samples = 1000
+
+
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):
+        fig = plt.figure(figsize=figsize)
+        # mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+
+        y.plot(ax=ts_ax)
+        ts_ax.set_title('Time Series Analysis Plots')
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return
+
+x = w = np.random.normal(size=n_samples)
+for t in range(n_samples):
+    x[t] = x[t-1] + w[t]
+
+_ = tsplot(np.diff(x), lags=30)
+np.savetxt('rand_walk.csv',x,delimiter=',')
+plt.show()
+
